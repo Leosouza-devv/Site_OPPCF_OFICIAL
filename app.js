ff8419c84438eee01b9988fd8cd828dbc3d92203
Leosouza-devv <106339564+leosouza-devv@users.noreply.github.com>
2025-09-03 09:42:17 -0300

    pross.desenvolvimento

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ const connection = require('./db');  // Importar a conexão com o banco de dados
 const app = express();
 
 // permite que arquivos com funções estaticas funcionem ao mesmo tempo que o node.js
-app.use(express.static('src/Arquivos_CSS&JS'));
-app.use(express.static('src/Arquivos_CSS&JS/CSS'));
+app.use(express.static('public/Arquivos_CSS&JS'));
+app.use(express.static('public'));
 
 //(INICIO) Rotas para as paginas HTML
 
 // Rota para pagina Inicio/index 
 app.get('/', (req, res) => {
-    
-  res.sendFile(__dirname + '/src/Arquivos_HTML/index.html'); // Vai servir o arquivo HTML
+  res.sendFile(__dirname + '/public/HTML/index.html'); // Vai servir o arquivo HTML
 });
+
 
 
 //(FIM) Rotas para as paginas HTML
