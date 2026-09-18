1de346ef5181763a867718ff2cf0ea4ab3a4570b
Dimitri D. Alonso <dimiduboc@gmail.com>
2026-09-18 00:34:40 -0300

    estrutura de arquivos

--- app.cjs
+++ app.cjs
@@ -2,10 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
 
-app.use(express.static('static'));
+app.use(express.static('public'));
+app.use(express.static('public/static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/static/index.html');
+  res.sendFile(__dirname + '/public/static/index.html');
 });
 
 app.listen(process.env.PORT || 3000, () => {
